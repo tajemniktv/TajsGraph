e9c59a8e3da8f05e23c48edd24934c67a4a19351
Gregory <tajemniktv@outlook.com>
2025-02-12 00:41:25 +0100

    Website
    Signed-off-by: Gregory <tajemniktv@outlook.com>

--- Website/starlight/tailwind/astro.config.mjs
+++ Website/starlight/tailwind/astro.config.mjs
@@ -1,12 +1,12 @@
+// @ts-check
 import {defineConfig} from 'astro/config';
 import starlight from '@astrojs/starlight';
-import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'Docs with Tailwind',
+            title: 'My Docs',
 			social: {
 				github: 'https://github.com/withastro/starlight',
 			},
@@ -15,16 +15,14 @@ export default defineConfig({
 					label: 'Guides',
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{label: 'Example Guide', slug: 'guides/example'},
+                        {label: 'Example Guide', slug: 'guides/example'},
 					],
 				},
 				{
 					label: 'Reference',
-					autogenerate: {directory: 'reference'},
+                    autogenerate: {directory: 'reference'},
 				},
 			],
-			customCss: ['./src/tailwind.css'],
 		}),
-		tailwind({applyBaseStyles: false}),
 	],
 });
